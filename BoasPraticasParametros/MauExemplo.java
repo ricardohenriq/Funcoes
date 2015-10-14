155c03c20cd35dd3a3ae8830d9a30e1f7faa482a
Samuel Junio de Almeida <almeida.samuel.junio@gmail.com>
2015-10-14 18:43:32 -0300

    Bons e Maus Exemplos - Parâmetros
    Bons e Maus Exemplos - Parâmetros

---
+++ BoasPraticasParametros/MauExemplo.java
@@ -0,0 +1,11 @@
+public class Despachante{
+	/*
+		No método enviarRemessa escrito abaixo
+		podemos ver que os parâmetros estão sendo 
+		repassados de forma solta e não coesa.
+	*/
+	public void enviarRemessa(String rua, int numero, String cidade, String estado, double valor){
+		/*...*/
+	}
+}
+
